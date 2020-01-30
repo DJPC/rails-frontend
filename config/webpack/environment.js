6350754f42d0f473746b5fdd655f9ad71b8c519e
Donna Cadiente <djanecad@hotmail.com>
2020-01-30 15:49:25 +0000

    front end following video exercise

---
+++ config/webpack/environment.js
@@ -0,0 +1,10 @@
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
+
+module.exports = environment
